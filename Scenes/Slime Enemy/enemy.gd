6db614565de5aff357df974c9c10a4688567a48d
bigtroublejake <bigtroublejake@gmail.com>
2024-01-10 19:10:18 -0500

    Updated slime added animations + updated movement physics

--- Scenes/Slime Enemy/enemy.gd
+++ Scenes/Slime Enemy/enemy.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
 @export var speed : int 
+@export var acceleration : int
 var player_chase = false
 var player: CharacterBody2D
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -16,10 +19,28 @@ func gravity_handle(delta):
 
 func _physics_process(delta):
 	gravity_handle(delta)
-	move_and_slide()
+	print(velocity.x)
+	
 	if player_chase:
-		position.x = move_toward(position.x, player.position.x, speed * delta)
+		if player.position.x > position.x:
+			velocity.x = move_toward(velocity.x, speed, acceleration * delta * 60)
+			animated_sprite_2d.flip_h = false
+		if player.position.x < position.x:
+			velocity.x = move_toward(velocity.x, -speed, acceleration * delta * 60)
+			animated_sprite_2d.flip_h = true
+	else:
+		velocity.x = move_toward(velocity.x, 0, acceleration / 2 * delta * 60)
+	
+	move_and_slide()
+	
+	_update_animation()
 
+
+func _update_animation():
+	if velocity.x != 0:
+		animated_sprite_2d.play("move")
+	elif velocity.x == 0:
+		animated_sprite_2d.play("idle")
 
 func _on_detection_area_body_entered(body):
 	if body != null:
